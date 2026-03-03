901999b4619af698181dd355cde74b99be19a60b
mtobesyajerry-sudo <mtobesya.jerry@gmail.com>
2026-03-03 22:51:39 +0300

    Updated Auth.jsx

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -416,7 +416,6 @@ export default function Auth() {
                 fontSize: '14px',
                 color: '#4a90e2',
                 textAlign: 'center',
-                fontWeight: '600',
                 fontStyle: 'italic'
               }}>
                 Don't have an account yet? Please Register to have access to the System.
