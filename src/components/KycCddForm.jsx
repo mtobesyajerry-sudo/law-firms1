1d395c0c571e4d2607d270e40286144e32e409c0
mtobesyajerry-sudo <mtobesya.jerry@gmail.com>
2026-07-05 17:06:12 +0300

    Updated KycCddForm.jsx

--- src/components/KycCddForm.jsx
+++ src/components/KycCddForm.jsx
@@ -74,7 +74,7 @@ export default function KycCddForm({ recordId: propRecordId, onSave, onCancel })
       if (error) throw error;
 
       if (data) {
-        setCustomerType(data.customer_type);
+        setCustomerType(data.customer_type || (data.client_type === 'legal_entity' ? 'legal_entity' : 'natural_person'));
         if (data.matter_id) {
           setMatterId(data.matter_id);
           loadMatter(data.matter_id);
